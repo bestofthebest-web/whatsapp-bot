6206d4123b7f41df7b24109b317d60dadd2ece99
bestofthebest-web <personifiedrobin@yahoo.com>
2025-05-24 01:04:03 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit of WhatsApp bot"
+git branch -M main
+git remote add origin https://github.com/bestofthebest/whatsapp-bot.git
+git push -u origin main
